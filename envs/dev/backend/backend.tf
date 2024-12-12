9f3f65842679c70175bebcb7a2aaf27d8459fa85
felipeBarras <felipeu_santos@hotmail.com>
2024-12-11 21:53:27 -0300

    ajeitando backend

--- envs/dev/backend/backend.tf
+++ envs/dev/backend/backend.tf
@@ -3,7 +3,5 @@ terraform {
     bucket = "postech-backend-tf"
     key    = "s3/post-8soat/terraform.tfstate"
     region = "us-east-1"
-    access_key = var.aws_access_key_id
-    secret_key = var.aws_secret_access_key
   }
 }
--- envs/dev/backend/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "aws_access_key_id" {
-    default = "AKIAST6S7GAEHFYY4EPI"
-}
-variable "aws_secret_access_key" {
-    default = "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"
-}
