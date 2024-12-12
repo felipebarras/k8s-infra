176fb682f9abf70995e7878bd92ba7e2b3601f05
felipeBarras <felipeu_santos@hotmail.com>
2024-12-11 21:39:52 -0300

    Preciso de férias

--- envs/dev/backend/backend.tf
+++ envs/dev/backend/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "postech-backend-tf"
     key    = "s3/post-8soat/terraform.tfstate"
     region = "us-east-1"
+    access_key = "AKIAST6S7GAEHFYY4EPI"
+    secret_key = "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"
   }
 }
