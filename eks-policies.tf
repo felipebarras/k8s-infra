1e33071c4033804ab31bf9f5982f4a67ed2335a8
felipeBarras <felipeu_santos@hotmail.com>
2024-12-13 09:18:08 -0300

    Adicionando mudanças de Services direto no Terraform

--- eks-policies.tf
+++ eks-policies.tf
@@ -1,36 +1,14 @@
-# Policy para o eks_user
-resource "aws_iam_policy" "eks_user_policy" {
-  name        = "eks_user_policy"
-  description = "Policy for EKS User to manage roles and resources"
-  policy      = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = [
-          "sts:AssumeRole",
-          "iam:GetRole",
-          "iam:ListRoles",
-          "iam:ListRolePolicies",
-          "iam:ListAttachedRolePolicies",
-          "iam:ListEntitiesForPolicy",
-          "iam:AttachRolePolicy",
-          "iam:PutRolePolicy",
-          "iam:PassRole",
-          "iam:DetachRolePolicy",
-          "iam:CreateRole",
-          "iam:DeleteRole",
-          "iam:CreatePolicy",
-          "ec2:CreateSecurityGroup",
-          "ec2:AuthorizeSecurityGroupIngress",
-          "ec2:AuthorizeSecurityGroupEgress",
-          "ec2:RevokeSecurityGroupEgress"
-        ],
-        Resource = [
-          "arn:aws:iam::180294201352:role/*",
-          "arn:aws:ec2:us-east-1:180294201352:security-group/*"
-        ]
-      }
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+    role = aws_iam_role.eks_node_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+    role = aws_iam_role.eks_node_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_registry_dead_policy" {
+    role = aws_iam_role.eks_node_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
--- eks-roles.tf
+++ eks-roles.tf
@@ -4,19 +4,12 @@ resource "aws_iam_role" "eks_service_role" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole",
         Effect = "Allow",
         Principal = {
           Service = "eks.amazonaws.com"
         }
-      },
-      {
         Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "arn:aws:iam::180294201352:user/eks_user"
-        }
-      }
+      },
     ]
   })
 }
@@ -27,25 +20,12 @@ resource "aws_iam_role" "eks_node_role" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole",
         Effect = "Allow",
         Principal = {
           Service = "ec2.amazonaws.com"
         }
-      },
-      {
         Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "arn:aws:iam::180294201352:user/eks_user"
-        }
       }
     ]
   })
-}
-
-# adicionando políticas às Roles
-resource "aws_iam_role_policy_attachment" "eks_service_role_policy_attachment" {
-  role = aws_iam_role.eks_service_role.name
-  policy_arn = aws_iam_policy
 }
